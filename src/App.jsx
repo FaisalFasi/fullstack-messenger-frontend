47581bc49784bdad5f0054fe62a3d7784dfe8c92
faisalrehman <faisalfasi18@gmail.com>
2023-07-19 16:12:05 +0200

    Messages are deleting and updating

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,131 @@ import { useEffect, useState } from "react";
 function App() {
   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:9000/messages")
-      .then((res) => res.json())
-      .then((data) => setMessages(data));
-  }, []);
-  useEffect(() => {
-    console.log(messages);
-  }, [messages]);
+  const [usernameInput, setUsernameInput] = useState("");
+  const [contentInput, setContentInput] = useState("");
 
+  const [editingId, setEditingId] = useState(null);
+  const [temporaryEditingContent, setTemporaryEditingContent] = useState(""); // TEMPORARY STATE FOR EDITING
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch("http://localhost:9000/messages", {
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .then((data) => setMessages(data));
+    }, [1000]);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const message = {
+      content: contentInput,
+      username: usernameInput,
+    };
+
+    fetch("http://localhost:9000/messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("new Document received from server", data);
+        setMessages([...messages, data]);
+      });
+  };
+
+  // PUT MESSAGE
+  const startOrFinishEditing = (id) => {
+    if (editingId === id) {
+      finishEditingAndSaveChangesInServer();
+    } else {
+      startEditing(id);
+    }
+  };
+
+  const startEditing = (id) => {
+    setEditingId((previous) => (previous === id ? null : id));
+    setTemporaryEditingContent(
+      messages.find((message) => message.id === id).content
+    );
+  };
+
+  const finishEditingAndSaveChangesInServer = () => {
+    setEditingId(null);
+    fetch(`http://localhost:9000/messages/${editingId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ content: temporaryEditingContent }),
+    })
+      .then((res) => res.json())
+      .then((updatedMessageFromBackend) => {
+        setMessages(
+          messages.map((message) =>
+            message.id === editingId ? updatedMessageFromBackend : message
+          )
+        );
+      });
+  };
+
+  // DELETE MESSAGE
+  const handleDelete = (id) => {
+    fetch(`http://localhost:9000/messages/${id}`, {
+      method: "DELETE",
+    }).then((response) => {
+      if (response.ok && response.status === 204) {
+        setMessages(messages.filter((message) => message.id !== id));
+      }
+    });
+  };
   return (
     <>
+      <h1>editingId: {editingId ? editingId : "null"}</h1>
       {messages
-        .sort((a, b) => a.created - b.created)
+        .sort((a, b) => a.created_at - b.created_at)
         .map((message) => {
           return (
             <div key={message.id}>
-              {message.created}
-              {message.username} says {message.text}
+              {message.username}:{" "}
+              {editingId === message.id ? (
+                <input
+                  value={temporaryEditingContent}
+                  onChange={(e) => setTemporaryEditingContent(e.target.value)}
+                  name="content"
+                  type="text"
+                />
+              ) : (
+                <div>{message.content}</div>
+              )}
+              <button onClick={() => handleDelete(message.id)}>🚮</button>
+              <button onClick={() => startOrFinishEditing(message.id)}>
+                🔧
+              </button>
             </div>
           );
         })}
-      <h1>Vite + React</h1>
+
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          value={usernameInput}
+          onChange={(e) => setUsernameInput(e.target.value)}
+          name="username"
+          type="text"
+        />
+        <input
+          value={contentInput}
+          onChange={(e) => setContentInput(e.target.value)}
+          name="content"
+          type="text"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
