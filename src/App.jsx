baf383d700fbee5796c81fb79e05e0d8221c7f77
faisalrehman <faisalfasi18@gmail.com>
2023-07-20 13:51:23 +0200

    removed process bug

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   useEffect(() => {
     setInterval(() => {
-      fetch(`${import.meta.process.env.VITE_MESSAGING_API}/messages`, {
+      fetch(`${import.meta.env.VITE_MESSAGING_API}/messages`, {
         method: "GET",
       })
         .then((res) => {
@@ -38,7 +38,7 @@ function App() {
       username: usernameInput,
     };
 
-    fetch(`${import.meta.process.env.VITE_MESSAGING_API}/messages`, {
+    fetch(`${import.meta.env.VITE_MESSAGING_API}/messages`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -78,16 +78,13 @@ function App() {
 
   const finishEditingAndSaveChangesInServer = () => {
     setEditingId(null);
-    fetch(
-      `${import.meta.process.env.VITE_MESSAGING_API}/messages/${editingId}`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ content: temporaryEditingContent }),
-      }
-    )
+    fetch(`${import.meta.env.VITE_MESSAGING_API}/messages/${editingId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ content: temporaryEditingContent }),
+    })
       .then((res) => {
         if (!res.ok) {
           throw new Error(
@@ -108,7 +105,7 @@ function App() {
 
   // DELETE MESSAGE
   const handleDelete = (id) => {
-    fetch(`${import.meta.process.env.VITE_MESSAGING_API}/messages/${id}`, {
+    fetch(`${import.meta.env.VITE_MESSAGING_API}/messages/${id}`, {
       method: "DELETE",
     }).then((response) => {
       if (response.ok && response.status === 204) {
